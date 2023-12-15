1f1aefae79f476bf2f9c0723f3bfcc0c85f30eb6
Jeff Capone <jeffcap1@gmail.com>
2023-12-15 03:25:30 -0500

    :sparkles: feat: Converting config to Lua and more modern tooling

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ These are the settings and keybindings for nvim. Also contains the Plugins and c
 This is the extension used to allow Neo Vim functional within VSCode. All of the configurations are
 set for use with this extension.
 
+## References
+- [typecraft_dev](https://www.youtube.com/watch?v=zHTeCSVAFNY&list=PLsz00TDipIffreIaUNk64KxTIkQaGguqn)
+- [chris@machine](https://www.youtube.com/watch?v=ctH-a-1eUME&list=PLhoH5vyxr6Qq41NFL4GvhFp-WLd5xzIzZ)
+
---
+++ init.lua
@@ -0,0 +1,17 @@
+-- setup Lazy package manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- include different lua config files
+require("vim-settings")
+require("lazy").setup("plugins")
