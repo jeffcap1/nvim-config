862412bc84e8901c7425a75efd3a212c5639dfaf
Jeff Capone <jeffcap1@gmail.com>
2024-01-07 20:49:35 -0500

    :sparkles: feat: Added snippets and completions and autoformatting on save

--- lua/user/plugins/lsp-config.lua
+++ lua/user/plugins/lsp-config.lua
@@ -1,34 +1,60 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end
-  },
-  {
-    'williamboman/mason-lspconfig.nvim',
-    config = function()
-      require('mason-lspconfig').setup({
-        ensure_installed = { "lua_ls", "tsserver" }
-      })
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      local lspconfig = require('lspconfig')
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "tsserver" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      -- setup language server
-      lspconfig.lua_ls.setup({})
-      lspconfig.tsserver.setup({})
+			local lspconfig = require("lspconfig")
 
-      -- lsp keybindings
-      vim.keymap.set('n', 'gh', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
-      vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
-      vim.keymap.set({ 'n', 'v' }, 'ga', vim.lsp.buf.code_action, {})
-    end
-  }
+			-- setup language server
+			local servers = { "lua_ls", "intelephense", "jsonls", "html", "tsserver" }
+			for _, lsp in ipairs(servers) do
+				lspconfig[lsp].setup({
+					capabilities = capabilities,
+				})
+			end
+
+			-- lsp keybindings
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+				callback = function(ev)
+					-- Buffer local mappings.
+					-- See `:help vim.lsp.*` for documentation on any of the below functions
+					local opts = { buffer = ev.buf }
+					vim.keymap.set("n", "gh", vim.lsp.buf.hover, opts)
+					vim.keymap.set("n", "gH", vim.lsp.buf.signature_help, opts)
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+					vim.keymap.set({ "n", "v" }, "ga", vim.lsp.buf.code_action, opts)
+					vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+					vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+
+					-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+					vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+					vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+					vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+					vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
+				end,
+			})
+		end,
+	},
 }
--- lua/user/plugins/none-ls.lua
+++ lua/user/plugins/none-ls.lua
@@ -1,21 +1,35 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				-- linters
-				null_ls.builtins.diagnostics.eslint_d,
-				null_ls.builtins.diagnostics.phpcs,
-				null_ls.builtins.diagnostics.ruff,
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format()
+            end,
+          })
+        end
+      end,
+      sources = {
+        -- linters
+        null_ls.builtins.diagnostics.eslint_d,
+        null_ls.builtins.diagnostics.phpcs,
+        null_ls.builtins.diagnostics.ruff,
 
-				-- formatters
-				null_ls.builtins.formatting.htmlbeautifier,
-				null_ls.builtins.formatting.phpcsfixer,
-				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.formatting.stylua,
-			},
-		})
-		vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format, {})
-	end,
+        -- formatters
+        null_ls.builtins.formatting.htmlbeautifier,
+        null_ls.builtins.formatting.phpcsfixer,
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.stylua,
+      },
+    })
+    vim.keymap.set("n", "<leader>ff", function()
+      vim.lsp.buf.format({ async = true })
+    end, {})
+  end,
 }
