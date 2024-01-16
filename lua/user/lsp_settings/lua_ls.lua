a77cfb8704de70f34b36dd04cfacae3d209bfec1
Jeff Capone <jeffcap1@gmail.com>
2024-01-16 01:49:53 -0500

    :sparkles: feat: Added more to Lua LSP Config

--- lua/user/lsp_settings/lua_ls.lua
+++ lua/user/lsp_settings/lua_ls.lua
@@ -1,5 +1,6 @@
+-- https://luals.github.io/wiki/settings/
 return {
-  settings({
+  settings = {
     Lua = {
       format = {
         enable = true,
@@ -11,6 +12,25 @@ return {
       diagnostics = {
         globals = { "vim", "spec" },
       },
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+          [vim.fn.stdpath("config") .. "/lua"] = true,
+        },
+      },
+      hint = {
+        enable = false,
+        arrayIndex = "Disable", -- "Enable" | "Auto" | "Disable"
+        await = true,
+        paramName = "Disable", -- "All" | "Literal" | "Disable"
+        paramType = true,
+        semicolon = "All", -- "All" | "SameLine" | "Disable"
+        setType = false,
+      },
+      telemetry = {
+        enable = false,
+      },
     },
-  }),
+  },
 }
