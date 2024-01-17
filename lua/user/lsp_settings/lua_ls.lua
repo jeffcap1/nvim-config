3b330f4df3dd1e96f6d8dd61565c423d5c1fe949
Jeff Capone <jeffcap1@gmail.com>
2024-01-17 00:15:20 -0500

    :sparkles: feat: Better code completion popup

--- lua/user/lsp_settings/lua_ls.lua
+++ lua/user/lsp_settings/lua_ls.lua
@@ -12,6 +12,9 @@ return {
       diagnostics = {
         globals = { "vim", "spec" },
       },
+      runtime = {
+        version = "LuaJIT",
+      },
       workspace = {
         checkThirdParty = false,
         library = {
@@ -20,10 +23,10 @@ return {
         },
       },
       hint = {
-        enable = false,
+        enable = true,
         arrayIndex = "Disable", -- "Enable" | "Auto" | "Disable"
         await = true,
-        paramName = "Disable", -- "All" | "Literal" | "Disable"
+        paramName = "All", -- "All" | "Literal" | "Disable"
         paramType = true,
         semicolon = "All", -- "All" | "SameLine" | "Disable"
         setType = false,
