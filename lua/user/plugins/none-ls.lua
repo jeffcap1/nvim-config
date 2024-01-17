ebd7caf6eb041e71f1d36eee587a2ff62b499be3
Jeff Capone <jeffcap1@gmail.com>
2024-01-17 00:17:11 -0500

    :sparkles: feat: Better format to file for none-ls and updated plugins

--- lua/user/plugins/none-ls.lua
+++ lua/user/plugins/none-ls.lua
@@ -1,36 +1,48 @@
-return {
+local M = {
   "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-          local augroup = vim.api.nvim_create_augroup("auto_format", {})
-          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              vim.lsp.buf.format()
-            end,
-          })
-        end
-      end,
-      sources = {
-        -- linters
-        null_ls.builtins.diagnostics.eslint_d,
-        null_ls.builtins.diagnostics.phpcs,
-        null_ls.builtins.diagnostics.ruff,
-
-        -- formatters
-        null_ls.builtins.formatting.htmlbeautifier,
-        null_ls.builtins.formatting.phpcsfixer,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.stylua,
-      },
-    })
-    vim.keymap.set("n", "<leader>lf", function()
-      vim.lsp.buf.format({ async = true })
-    end, { desc = "LSP format" })
-  end,
 }
+
+function M.auto_format(client, bufnr)
+  if client.supports_method("textDocument/formatting") then
+    local augroup = vim.api.nvim_create_augroup("auto_format", {})
+    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = augroup,
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format()
+      end,
+    })
+  end
+end
+
+function M.config()
+  local null_ls = require("null-ls")
+
+  local formatting = null_ls.builtins.formatting
+  local diagnostics = null_ls.builtins.diagnostics
+
+  null_ls.setup({
+    debug = false,
+    on_attach = function(client, bufnr)
+      M.auto_format(client, bufnr)
+    end,
+    sources = {
+      -- linters
+      diagnostics.eslint_d,
+      diagnostics.phpcs,
+      diagnostics.ruff,
+
+      -- formatters
+      formatting.htmlbeautifier,
+      formatting.phpcsfixer,
+      formatting.prettier,
+      formatting.stylua,
+    },
+  })
+  vim.keymap.set("n", "<leader>lf", function()
+    vim.lsp.buf.format({ async = true })
+  end, { desc = "LSP format" })
+end
+
+return M
