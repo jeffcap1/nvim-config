ce4697fb56426b44af7d3617ae62502130cd9a02
jeffcap1 <jeffcap1@gmail.com>
2024-05-06 15:23:34 -0400

    :bug: fix: Resolved prettier and ruff issue

--- lua/user/plugins/none-ls.lua
+++ lua/user/plugins/none-ls.lua
@@ -5,9 +5,9 @@ local M = {
 local autoformat = true
 
 function M.auto_format(client, bufnr)
-  if client.supports_method "textDocument/formatting" then
+  if client.supports_method("textDocument/formatting") then
     local augroup = vim.api.nvim_create_augroup("auto_format", { clear = true })
-    vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
     vim.api.nvim_create_autocmd("BufWritePre", {
       group = augroup,
       buffer = bufnr,
@@ -21,12 +21,12 @@ function M.auto_format(client, bufnr)
 end
 
 function M.config()
-  local null_ls = require "null-ls"
+  local null_ls = require("null-ls")
 
   local formatting = null_ls.builtins.formatting
   local diagnostics = null_ls.builtins.diagnostics
 
-  null_ls.setup {
+  null_ls.setup({
     debug = false,
     on_attach = function(client, bufnr)
       M.auto_format(client, bufnr)
@@ -34,23 +34,40 @@ function M.config()
     sources = {
       -- linters
       diagnostics.phpcs,
-      diagnostics.ruff,
 
       -- formatters
-      formatting.htmlbeautifier,
-      formatting.phpcsfixer,
-      formatting.prettier,
+      -- formatting.htmlbeautifier,
       formatting.stylua,
       formatting.sql_formatter,
+      formatting.phpcsfixer,
+      formatting.prettier.with({
+        filetypes = {
+          "css",
+          "scss",
+          "less",
+          "html",
+          "markdown",
+          "json",
+          "yaml",
+          "lua",
+          "astro",
+          "vue",
+          "svelte",
+          "javascript",
+          "typescript",
+          "javascriptreact",
+          "typescriptreact",
+        },
+      }),
     },
-  }
+  })
 
   vim.keymap.set("n", "<leader>lf", function()
-    vim.lsp.buf.format { async = true }
-  end, XTND { desc = "LSP format" })
+    vim.lsp.buf.format({ async = true })
+  end, XTND({ desc = "LSP format" }))
   vim.keymap.set("n", "<leader>lt", function()
     autoformat = not autoformat
-  end, XTND { desc = "LSP toggle autoformat" })
+  end, XTND({ desc = "LSP toggle autoformat" }))
 end
 
 return M
