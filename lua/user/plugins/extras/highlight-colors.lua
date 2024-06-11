29234b24ef5a78f19c0fc84514fbe437034a29eb
jeffcap1 <jeffcap1@gmail.com>
2024-06-11 16:56:17 -0400

    :sparkles: feat: Added exclude filetypes for color highlighter

--- lua/user/plugins/extras/highlight-colors.lua
+++ lua/user/plugins/extras/highlight-colors.lua
@@ -9,27 +9,37 @@ function M.config()
     virtual_symbol = "■",
     enable_named_colors = false,
     enable_tailwind = true,
+    exclude_filetypes = {
+      "mason",
+      "help",
+      "startify",
+      "dashboard",
+      -- "markdown"
+      "harpoon",
+      "DressingInput",
+      "NeogitCommitMessage",
+      "qf",
+      "dirvish",
+      "oil",
+      "minifiles",
+      "fugitive",
+      "alpha",
+      "NvimTree",
+      "lazy",
+      "NeogitStatus",
+      "Trouble",
+      "netrw",
+      "lir",
+      "DiffviewFiles",
+      "Outline",
+      "Jaq",
+      "spectre_panel",
+      "toggleterm",
+      "DressingSelect",
+      "TelescopePrompt",
+    }
   }
 
-  -- require("colorizer").setup({
-  --   filetypes = {
-  --     "typescript",
-  --     "typescriptreact",
-  --     "javascript",
-  --     "javascriptreact",
-  --     "css",
-  --     "html",
-  --     "astro",
-  --     "lua",
-  --   },
-  --   user_default_options = {
-  --     names = false,
-  --     rgb_fn = true,
-  --     hsl_fn = true,
-  --     tailwind = "both",
-  --   },
-  --   buftypes = {},
-  -- })
 end
 
 return M
