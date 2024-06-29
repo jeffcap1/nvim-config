d164d9d5a83e55a1bbca67cf962a0edd14b826d3
Jeff Capone <jeffcap1@gmail.com>
2024-06-29 15:43:03 -0400

    :wrench: chore: Updated Lazy

--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -1,18 +1,18 @@
 -- setup Lazy package manager
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system {
     "git",
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
     "--branch=stable", -- latest stable release
     lazypath,
-  })
+  }
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
+require "lazy".setup {
   spec = LAZY_PLUGIN_SPEC,
   install = {
     colorscheme = { "catppuccin", "default" },
@@ -20,9 +20,12 @@ require("lazy").setup({
   ui = {
     border = "rounded",
   },
+  checker = {
+    enabled = true,
+  },
   change_detection = {
     -- automatically check for config file changes and reload the ui
     enabled = true,
     notify = false, -- get a notification when changes are found
   },
-})
+}
