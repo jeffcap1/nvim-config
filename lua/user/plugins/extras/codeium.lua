7accb396b9ee8157a92deb8627ce32677c274333
Jeff Capone <jeffcap1@gmail.com>
2024-09-10 01:02:55 -0400

    :sparkles: feat: Switched to new Codeium plugin

--- lua/user/plugins/extras/codeium.lua
+++ lua/user/plugins/extras/codeium.lua
@@ -1,42 +1,41 @@
 local M = {
-  "Exafunction/codeium.vim",
-  cmd = "Codeium",
-  event = "BufEnter",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "hrsh7th/nvim-cmp",
-  },
+  "monkoose/neocodeium",
+  event = "VeryLazy",
 }
 
 M.config = function()
-  vim.g.codeium_disable_bindings = 1
-  vim.g.codeium_enable_chat = 1
-  vim.g.codeium_filetypes = {
-    markdown = true,
-    help = false,
-    gitcommit = false,
-    gitrebase = false,
-    hgcommit = false,
-    svn = false,
-    cvs = false,
-    ["."] = false,
+  local neocodeium = require "neocodeium"
+
+  neocodeium.setup {
+    filetypes = {
+      -- markdown = true,
+      help = false,
+      gitcommit = false,
+      gitrebase = false,
+      hgcommit = false,
+      svn = false,
+      cvs = false,
+      TelescopePrompt = false,
+      ["dap-repl"] = false,
+      ["."] = false,
+    },
   }
 
   vim.keymap.set("i", "<C-l>", function()
     vim.notify "accepting codeium suggestion"
-    return vim.fn["codeium#Accept"]()
+    return neocodeium.accept
   end, { expr = true, silent = true })
 
   vim.keymap.set("i", "<c-n>", function()
-    return vim.fn["codeium#CycleCompletions"](1)
+    return neocodeium.cycle_or_complete()
   end, { expr = true, silent = true })
 
   vim.keymap.set("i", "<c-p>", function()
-    return vim.fn["codeium#CycleCompletions"](-1)
+    return neocodeium.cycle_or_complete(-1)
   end, { expr = true, silent = true })
 
   vim.keymap.set("i", "<c-h>", function()
-    return vim.fn["codeium#Clear"]()
+    return neocodeium.clear()
   end, { expr = true, silent = true })
 end
 
--- lua/user/plugins/lualine.lua
+++ lua/user/plugins/lualine.lua
@@ -2,13 +2,42 @@ local M = {
   "nvim-lualine/lualine.nvim",
   dependencies = {
     "AndreM222/copilot-lualine",
+    "monkoose/neocodeium",
   },
 }
 
+-- NeoCodeium Status
+-- @see https://www.reddit.com/r/neovim/comments/1fc34na/comment/lm5wr1j/?rdt=59883
+function get_codeium_status()
+  local status, serverstatus = require "neocodeium".get_status()
+
+  -- Tables to map serverstatus and status to corresponding symbols
+  local server_status_symbols = {
+    [0] = "󰣺 ", -- Connected
+    [1] = "󱤚 ", -- Connecting
+    [2] = "󰣽 ", -- Disconnected
+  }
+
+  local status_symbols = {
+    [0] = "󰚩 ", -- Enabled
+    [1] = "󱚧 ", -- Disabled Globally
+    [3] = "󱚢 ", -- Disabled for Buffer filetype
+    [5] = "󱚠 ", -- Disabled for Buffer encoding
+    [2] = "󱙻 ", -- Disabled for Buffer (catch-all)
+  }
+
+  -- Handle serverstatus and status fallback (safeguard against any unexpected value)
+  local luacodeium = server_status_symbols[serverstatus] or "󰣼 "
+  luacodeium = luacodeium .. (status_symbols[status] or "󱚧 ")
+
+  return luacodeium
+end
+
 function M.config()
   local function show_codeium_status()
-    if vim.g.loaded_codeium then
-      return "{…}" .. vim.fn["codeium#GetStatusString"]()
+    local status = get_codeium_status()
+    if status ~= "󰣼 󱚧 " then
+      return status
     else
       return ""
     end
@@ -22,18 +51,17 @@ function M.config()
     end
   end
 
-  require("lualine").setup({
+  require "lualine".setup {
     options = {
       -- theme = "dracula",
       theme = "catppuccin",
     },
 
-    { show_codeium_status },
     sections = {
       lualine_y = { show_copilot_status, show_codeium_status, "progress", "selectioncount" },
       lualine_z = { "location" },
     },
-  })
+  }
 end
 
 return M
