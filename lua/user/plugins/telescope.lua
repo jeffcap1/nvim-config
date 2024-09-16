17d14265b2c0146b9549e464c835efb8ce3181bb
Jeff Capone <jeffcap1@gmail.com>
2024-09-16 14:03:47 -0400

    feat: Improved telescope commands

--- lua/user/plugins/telescope.lua
+++ lua/user/plugins/telescope.lua
@@ -9,35 +9,33 @@ local M = {
 }
 
 function M.config()
-  local builtin = require "telescope.builtin"
+  local builtin = require("telescope.builtin")
+  local extensions = require("telescope").extensions
   local keymap = vim.keymap.set
 
-  keymap("n", "<leader>fp", builtin.git_files, XTND { desc = "Git Files" })
-  keymap("n", "<leader>bb", builtin.buffers, XTND { desc = "Buffers" })
+  keymap("n", "<leader>fp", builtin.git_files, XTND({ desc = "Git Files" }))
+  keymap("n", "<leader>bb", builtin.buffers, XTND({ desc = "Buffers" }))
   keymap("n", "<leader>fa", function()
-    builtin.find_files { hidden = true, no_ignore = true }
-  end, XTND { desc = "Find All Files" })
-  keymap("n", "<leader>fb", builtin.current_buffer_fuzzy_find, XTND { desc = "Telescope Find Buffer" })
-  keymap("n", "<leader>ff", builtin.find_files, XTND { desc = "Find Files" })
-  keymap("n", "<leader>ft", builtin.live_grep, XTND { desc = "Find Text" })
-  keymap("n", "<leader>fs", builtin.grep_string, XTND { desc = "Find String" })
-  keymap("n", "<leader>fh", builtin.help_tags, XTND { desc = "Help" })
-  keymap(
-    "n",
-    "<leader>fi",
-    "<cmd>lua require('telescope').extensions.media_files.media_files()<cr>",
-    XTND { desc = "Media" }
-  )
-  keymap("n", "<leader>fl", builtin.resume, XTND { desc = "Resume" })
-  keymap("n", "<leader>fM", builtin.man_pages, XTND { desc = "Man Pages" })
-  keymap("n", "<leader>fr", builtin.oldfiles, XTND { desc = "Recent" })
-  keymap("n", "<leader>fR", builtin.registers, XTND { desc = "Registers" })
-  keymap("n", "<leader>fe", builtin.quickfix, XTND { desc = "Telescope Quick Fix" })
+    builtin.find_files({ hidden = true, no_ignore = true })
+  end, XTND({ desc = "Find All Files" }))
+  keymap("n", "<leader>fb", builtin.current_buffer_fuzzy_find, XTND({ desc = "Telescope Find Buffer" }))
+  keymap("n", "<leader>ff", builtin.find_files, XTND({ desc = "Find Files" }))
+  keymap("n", "<leader>ft", builtin.live_grep, XTND({ desc = "Find Text" }))
+  keymap("n", "<leader>fs", builtin.grep_string, XTND({ desc = "Find String" }))
+  keymap("n", "<leader>fh", builtin.help_tags, XTND({ desc = "Help" }))
+  keymap("n", "<leader>fi", extensions.media_files.media_files, XTND({ desc = "Media" }))
+  keymap("n", "<leader>fl", builtin.resume, XTND({ desc = "Resume" }))
+  keymap("n", "<leader>fm", builtin.man_pages, XTND({ desc = "Man Pages" }))
+  keymap("n", "<leader>fM", builtin.marks, XTND({ desc = "Marks" }))
+  keymap("n", "<leader>fr", builtin.oldfiles, XTND({ desc = "Recent" }))
+  keymap("n", "<leader>fR", builtin.registers, XTND({ desc = "Registers" }))
+  keymap("n", "<leader>fq", builtin.quickfix, XTND({ desc = "Telescope Quick Fix" }))
+  keymap("n", "<leader>fQ", builtin.quickfixhistory, XTND({ desc = "Telescope QF History" }))
 
-  local icons = require "user.icons"
-  local actions = require "telescope.actions"
+  local icons = require("user.icons")
+  local actions = require("telescope.actions")
 
-  require "telescope".setup {
+  require("telescope").setup({
     defaults = {
       prompt_prefix = icons.ui.Telescope .. " ",
       selection_caret = icons.ui.Forward .. " ",
@@ -61,13 +59,13 @@ function M.config()
     },
     extensions = {
       ["ui-select"] = {
-        require "telescope.themes".get_dropdown {},
+        require("telescope.themes").get_dropdown({}),
       },
       fzf = {
-        fuzzy = true,                   -- false will only do exact matching
+        fuzzy = true, -- false will only do exact matching
         override_generic_sorter = true, -- override the generic sorter
-        override_file_sorter = true,    -- override the file sorter
-        case_mode = "smart_case",       -- or "ignore_case" or "respect_case"
+        override_file_sorter = true, -- override the file sorter
+        case_mode = "smart_case", -- or "ignore_case" or "respect_case"
       },
     },
     pickers = {
@@ -135,8 +133,8 @@ function M.config()
         },
       },
     },
-  }
-  require "telescope".load_extension "ui-select"
+  })
+  require("telescope").load_extension("ui-select")
 end
 
 return M
