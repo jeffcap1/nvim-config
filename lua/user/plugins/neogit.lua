00c8c63568b8ba6c104a276a4b3d6df8fd05a384
Jeff Capone <jeffcap1@gmail.com>
2024-10-29 23:39:03 -0400

    :sparkles: feat: Updated Neogit to use graphical git tree

--- lua/user/plugins/neogit.lua
+++ lua/user/plugins/neogit.lua
@@ -12,17 +12,19 @@ M.config = function()
   local wk = require("which-key")
   local neogit = require("neogit")
 
-  neogit.setup({})
+  neogit.setup({
+    graph_style = "unicode",
+  })
 
   wk.add({
-    { "<leader>gs", neogit.open,              desc = "Neogit Open" },
+    { "<leader>gs", neogit.open, desc = "Neogit Open" },
     { "<leader>gc", "<cmd>Neogit commit<cr>", desc = "Neogit Commit" },
-    { "<leader>gp", "<cmd>Neogit pull<cr>",   desc = "Neogit Pull" },
-    { "<leader>gP", "<cmd>Neogit push<cr>",   desc = "Neogit Push" },
+    { "<leader>gp", "<cmd>Neogit pull<cr>", desc = "Neogit Pull" },
+    { "<leader>gP", "<cmd>Neogit push<cr>", desc = "Neogit Push" },
   }, {
-    mode = "n",     -- NORMAL mode
+    mode = "n", -- NORMAL mode
     prefix = "<leader>",
-    silent = true,  -- use `silent` when creating keymaps
+    silent = true, -- use `silent` when creating keymaps
     noremap = true, -- use `noremap` when creating keymaps
   })
 end
