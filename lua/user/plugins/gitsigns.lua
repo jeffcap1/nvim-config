c0773c68c8bb1c66ca2211d24836ddd90410038e
Jeff Capone <jeffcap1@gmail.com>
2024-11-04 11:27:11 -0500

    :sparkles: feat: Moved keybindings for Gitsigns

--- lua/user/plugins/gitsigns.lua
+++ lua/user/plugins/gitsigns.lua
@@ -2,28 +2,25 @@ local M = {
   "lewis6991/gitsigns.nvim",
   cmd = "Gitsigns",
 }
+
+-- stylua: ignore start
+M.keys = {
+  { "<leader>gd", "<cmd>Gitsigns diffthis HEAD<cr>", desc = "Git Diff", silent = true },
+  { "<leader>gj", "<cmd>lua require 'gitsigns'.next_hunk()<cr>", desc = "Next Hunk", silent = true  },
+  { "<leader>gk", "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", desc = "Prev Hunk", silent = true  },
+  { "<leader>gl", "<cmd>lua require 'gitsigns'.blame_line()<cr>", desc = "Blame", silent = true  },
+  { "<leader>gL", "<cmd>lua require 'gitsigns'.toggle_current_line_blame()<cr>", desc = "Blame Inline", silent = true  },
+  { "<leader>gh", "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", desc = "Preview Hunk", silent = true  },
+  { "<leader>gH", "<cmd>lua require 'gitsigns'.preview_hunk_inline()<cr>", desc = "Preview Hunk Inline", silent = true  },
+  { "<leader>gr", "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", desc = "Reset Hunk", silent = true  },
+  { "<leader>gR", "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", desc = "Reset Buffer", silent = true  },
+  { "<leader>gS", "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", desc = "Stage Hunk", silent = true  },
+  { "<leader>gu", "<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>", desc = "Undo Stage Hunk", silent = true  },
+}
+-- stylua: ignore end
+
 M.config = function()
   local icons = require("user.icons")
-
-  local wk = require("which-key")
-  wk.add({
-    { "<leader>gd", "<cmd>Gitsigns diffthis HEAD<cr>", desc = "Git Diff" },
-    { "<leader>gj", "<cmd>lua require 'gitsigns'.next_hunk()<cr>", desc = "Next Hunk" },
-    { "<leader>gk", "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", desc = "Prev Hunk" },
-    { "<leader>gl", "<cmd>lua require 'gitsigns'.blame_line()<cr>", desc = "Blame" },
-    { "<leader>gL", "<cmd>lua require 'gitsigns'.toggle_current_line_blame()<cr>", desc = "Blame Inline" },
-    { "<leader>gh", "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", desc = "Preview Hunk" },
-    { "<leader>gH", "<cmd>lua require 'gitsigns'.preview_hunk_inline()<cr>", desc = "Preview Hunk Inline" },
-    { "<leader>gr", "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", desc = "Reset Hunk" },
-    { "<leader>gR", "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", desc = "Reset Buffer" },
-    { "<leader>gS", "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", desc = "Stage Hunk" },
-    { "<leader>gu", "<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>", desc = "Undo Stage Hunk" },
-  }, {
-    mode = "n", -- NORMAL mode
-    prefix = "<leader>",
-    silent = true, -- use `silent` when creating keymaps
-    noremap = true, -- use `noremap` when creating keymaps
-  })
 
   require("gitsigns").setup({
     signs = {
