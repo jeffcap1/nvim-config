5579bea659c30698c0f8634950820960e564f2f5
Jeff Capone <jeffcap1@gmail.com>
2025-01-02 14:06:47 -0500

    :sparkles: feat: Fixing markdown preview build command

--- lua/user/plugins/markdown-preview.lua
+++ lua/user/plugins/markdown-preview.lua
@@ -2,8 +2,13 @@ local M = {
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   ft = { "markdown" },
-  build = function()
-    vim.fn["mkdp#util#install"]()
+  build = function(plugin)
+    if vim.fn.executable("npx") then
+      vim.cmd("!cd " .. plugin.dir .. " && cd app && npx --yes yarn install && npm install")
+    else
+      vim.cmd([[Lazy load markdown-preview.nvim]])
+      vim.fn["mkdp#util#install"]()
+    end
   end,
 }
 
