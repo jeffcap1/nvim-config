b731f6cee02e78c15ffa65ef8172c51fdd7a80ff
Jeff Capone <jeffcap1@gmail.com>
2025-02-04 00:47:08 -0500

    :sparkles: feat: switching avante for codecompanion

---
+++ lua/user/plugins/extras/codecompanion.lua
@@ -0,0 +1,32 @@
+local M = {
+  "olimorris/codecompanion.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+}
+
+M.opts = {
+  strategies = {
+    chat = {
+      adapter = "gemini",
+    },
+  },
+  opts = {
+    -- Set debug logging
+    log_level = "DEBUG",
+  },
+  adapters = {
+    anthropic = function()
+      return require("codecompanion.adapters").extend("gemini", {
+        schema = {
+          model = {
+            default = "gemini-2.0-flash-exp",
+          },
+        },
+      })
+    end,
+  },
+}
+
+return M
