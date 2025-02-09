6ba663e84ab81499d45348166eb1d16f681be632
Jeff Capone <jeffcap1@gmail.com>
2025-02-08 22:42:38 -0500

    :sparkles: feat: Improving gitsigns functionality

--- lua/user/plugins/gitsigns.lua
+++ lua/user/plugins/gitsigns.lua
@@ -1,6 +1,5 @@
 local M = {
   "lewis6991/gitsigns.nvim",
-  -- cmd = "Gitsigns",
   event = { "BufReadPost", "BufWritePost", "BufNewFile" },
 }
 
@@ -20,33 +19,28 @@ M.keys = {
 }
 -- stylua: ignore end
 
-M.config = function()
-  local icons = require("user.icons")
-
-  require("gitsigns").setup({
-    signs = {
-      add = { text = icons.ui.BoldLineMiddle },
-      change = { text = icons.ui.BoldLineDashedMiddle },
-      delete = { text = icons.ui.TriangleShortArrowRight },
-      topdelete = { text = icons.ui.TriangleShortArrowRight },
-      changedelete = { text = icons.ui.BoldLineMiddle },
-    },
-    watch_gitdir = {
-      interval = 1000,
-      follow_files = true,
-    },
-    attach_to_untracked = true,
-    current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
-    update_debounce = 200,
-    max_file_length = 40000,
-    preview_config = {
-      border = "rounded",
-      style = "minimal",
-      relative = "cursor",
-      row = 0,
-      col = 1,
-    },
-  })
-end
+local icons = require("user.icons")
+M.opts = {
+  signs = {
+    add = { text = icons.ui.BoldLineMiddle },
+    change = { text = icons.ui.BoldLineDashedMiddle },
+    delete = { text = icons.ui.TriangleShortArrowRight },
+    topdelete = { text = icons.ui.TriangleShortArrowRight },
+    changedelete = { text = icons.ui.BoldLineMiddle },
+  },
+  watch_gitdir = {
+    follow_files = true,
+  },
+  numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
+  attach_to_untracked = true,
+  current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
+  max_file_length = 40000,
+  current_line_blame_opts = {
+    delay = 300,
+  },
+  preview_config = {
+    border = "rounded",
+  },
+}
 
 return M
