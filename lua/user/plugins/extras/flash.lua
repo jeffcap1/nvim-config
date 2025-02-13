8a099e6589fc6c709fbbc7fb6d03df211b25ec54
Jeff Capone <jeffcap1@gmail.com>
2025-02-13 00:18:58 -0500

    :sparkles: feat: Improving flash

--- lua/user/plugins/extras/flash.lua
+++ lua/user/plugins/extras/flash.lua
@@ -1,14 +1,16 @@
 local M = {
   "folke/flash.nvim",
-  event = { "BufReadPost", "BufNewFile" },
+  event = "VeryLazy",
 }
 
 -- stylua: ignore
 M.keys = {
-  { "<c-j>", function() require("flash").jump() end, mode = { "n", "x" }, desc = "Flash" },
+  { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+  { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
 }
 -- stylua: ignore end
 
+---@type Flash.Config
 M.opts = {
   modes = {
     char = {
