273865386b39ef4f00043fc7611ca83306e2f2d5
Jeff Capone <jeffcap1@gmail.com>
2025-02-13 00:19:10 -0500

    :sparkles: feat: Improving noice setup

--- lua/user/plugins/noice.lua
+++ lua/user/plugins/noice.lua
@@ -1,25 +1,26 @@
 local M = {
   "folke/noice.nvim",
   event = "VeryLazy",
-  opts = {
-    -- add any options here
-    lsp = {
-      -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-      override = {
-        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        ["vim.lsp.util.stylize_markdown"] = true,
-        ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-      },
-    },
-    presets = {
-      bottom_search = true, -- use a classic bottom cmdline for search
-      lsp_doc_border = true,
-      long_message_to_split = true,
-    },
-  },
   dependencies = {
     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
     "MunifTanjim/nui.nvim",
+  },
+}
+
+M.opts = {
+  -- add any options here
+  lsp = {
+    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+    override = {
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = true,
+      -- ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+    },
+  },
+  presets = {
+    bottom_search = true, -- use a classic bottom cmdline for search
+    lsp_doc_border = true,
+    long_message_to_split = true,
   },
 }
 
