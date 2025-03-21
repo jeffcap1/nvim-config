5108c3e0330050024cc2f6dce1ee8d4d341cef30
Jeff Capone <jeffcap1@gmail.com>
2025-03-21 16:39:44 -0400

    :bug: fix: Resolved issue with snyk-ls not working

--- lua/user/lsp_settings/snyk_ls.lua
+++ lua/user/lsp_settings/snyk_ls.lua
@@ -1,5 +1,8 @@
+local util = require("lspconfig.util")
+
 return {
   cmd = { "snyk-ls" },
+  root_dir = util.root_pattern(".git", ".snyk"),
   filetypes = {
     "go",
     "gomod",
@@ -14,7 +17,13 @@ return {
     "terraform-vars",
   },
   single_file_support = true,
+  settings = {},
+  -- Configuration from https://github.com/snyk/snyk-ls#configuration-1
   init_options = {
+    activateSnykCode = "true",
+    endpoint = vim.fn.getenv("SNYK_API_URL"),
+    snykCodeApi = vim.fn.getenv("SNYK_DEEPROXY_API_URL"),
+    organization = vim.fn.getenv("SNYK_ORG"),
     token = vim.fn.getenv("SNYK_TOKEN"),
   },
 }
