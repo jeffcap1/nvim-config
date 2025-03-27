1d1f26fdee2e607ce0784a4d09426f14b8db6b02
Jeff Capone <jeffcap1@gmail.com>
2025-03-27 03:21:08 -0400

    :bug: fix: Removing old formatting reference

--- lua/user/plugins/none-ls.lua
+++ lua/user/plugins/none-ls.lua
@@ -6,14 +6,10 @@ local M = {
 function M.config()
   local null_ls = require("null-ls")
 
-  -- local formatting = null_ls.builtins.formatting
   local diagnostics = null_ls.builtins.diagnostics
 
   null_ls.setup({
     debug = false,
-    on_attach = function(client, bufnr)
-      M.auto_format(client, bufnr)
-    end,
     sources = {
       -- linters
       diagnostics.phpcs,
