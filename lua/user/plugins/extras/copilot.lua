6f59ebe6d1c059315c109be5e53d44c638c24b13
Jeff Capone <jeffcap1@gmail.com>
2025-10-03 21:00:53 -0400

    :sparkles: feat: Updating copilot config

--- lua/user/plugins/extras/copilot.lua
+++ lua/user/plugins/extras/copilot.lua
@@ -3,48 +3,48 @@ local M = {
   cmd = "Copilot",
   event = "InsertEnter",
   dependencies = {
-    "zbirenbaum/copilot-cmp",
+    "copilotlsp-nvim/copilot-lsp",
+    -- fang2hou/blink-copilot
   },
 }
 
-function M.config()
-  require("copilot").setup({
-    panel = {
-      keymap = {
-        jump_next = "<c-j>",
-        jump_prev = "<c-k>",
-        accept = "<c-l>",
-        refresh = "r",
-        open = "<M-CR>",
-      },
+M.opts = {
+  panel = {
+    keymap = {
+      jump_next = "<c-j>",
+      jump_prev = "<c-k>",
+      accept = "<c-l>",
+      refresh = "r",
+      open = "<M-CR>",
     },
-    suggestion = {
-      enabled = true,
-      auto_trigger = true,
-      keymap = {
-        accept = "<c-l>",
-        next = "<c-j>",
-        prev = "<c-k>",
-        dismiss = "<c-h>",
-      },
+  },
+  suggestion = {
+    enabled = true,
+    auto_trigger = true,
+    keymap = {
+      accept = "<c-l>",
+      next = "<c-n>",
+      prev = "<c-p>",
+      dismiss = "<c-h>",
     },
-    filetypes = {
-      markdown = true,
-      help = false,
-      gitcommit = false,
-      gitrebase = false,
-      hgcommit = false,
-      svn = false,
-      cvs = false,
-      ["."] = false,
-    },
-    copilot_node_command = "node",
-  })
+  },
+  filetypes = {
+    markdown = true,
+    help = false,
+    gitcommit = false,
+    gitrebase = false,
+    hgcommit = false,
+    svn = false,
+    cvs = false,
+    ["."] = false,
+  },
+  copilot_node_command = vim.fn.expand("$HOME") .. "/.local/share/fnm/node-versions/v24.9.0/installation/bin/node",
+}
 
-  local opts = { noremap = true, silent = true }
-  vim.api.nvim_set_keymap("n", "<c-s>", ":lua require('copilot.suggestion').toggle_auto_trigger()<CR>", opts)
-
-  -- require("copilot_cmp").setup()
-end
+-- stylua: ignore
+M.keys = {
+  { "<c-s>", ":lua require('copilot.suggestion').toggle_auto_trigger()<CR>" },
+}
+-- stylua: ignore end
 
 return M
