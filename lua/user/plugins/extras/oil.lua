3b77a5cb09870a68cad4deb67c7f8fc94b708537
Jeff Capone <jeffcap1@gmail.com>
2025-10-23 11:17:07 -0400

    :bug: fix: Resolve oil issues from update

--- lua/user/plugins/extras/oil.lua
+++ lua/user/plugins/extras/oil.lua
@@ -10,6 +10,8 @@ M.keys = {
   { "-", "<CMD>Oil --float<CR>", desc = "Open parent directory" },
 }
 
+---@module 'oil'
+---@type oil.SetupOpts
 M.opts = {
   delete_to_trash = true,
   skip_confirm_for_simple_edits = true,
@@ -28,6 +30,7 @@ M.opts = {
   float = {
     max_height = 40,
     max_width = 200,
+    border = "rounded",
   },
 }
 
